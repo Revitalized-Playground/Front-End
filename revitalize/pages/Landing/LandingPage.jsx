2de382236061919e260c703c6c2113cab149c252
unknown <omars.e.995@gmail.com>
2019-09-11 20:40:14 -0700

    :lipstick: Working our prupose CTA styling

--- revitalize/pages/Landing/LandingPage.jsx
+++ revitalize/pages/Landing/LandingPage.jsx
@@ -15,7 +15,8 @@
            <div className="cta-container">
              <div className="cta">
                <h2 className="cta-title">
-                 A Modern approach to become <br/>a licensed trades professional
+                 A Modern approach to become <br />a licensed trades
+                 professional
                </h2>
                <p>
                  Debt free hands-on education to rebuilding local communities
@@ -29,32 +30,42 @@
            <div className="values">
              <p>Values that will direct you to a successful career</p>
              <div className="value-tiles">
-               <div className="tenacity"><h4>Tenacity</h4></div>
-               <div className="ingenuity"><h4>Ingenuity</h4></div>
-               <div className="growth"><h4>Growth</h4></div>
-               <div className="diligence"><h4>Diligence</h4></div>
-               <div className="resilience"><h4>Resilience</h4></div>
-               <div className="compassion"><h4>Compassion</h4></div>
+               <div className="tenacity">
+                 <h4>Tenacity</h4>
+               </div>
+               <div className="ingenuity">
+                 <h4>Ingenuity</h4>
+               </div>
+               <div className="growth">
+                 <h4>Growth</h4>
+               </div>
+               <div className="diligence">
+                 <h4>Diligence</h4>
+               </div>
+               <div className="resilience">
+                 <h4>Resilience</h4>
+               </div>
+               <div className="compassion">
+                 <h4>Compassion</h4>
+               </div>
              </div>
            </div>
 
            <div className="applicaton-links-container">
-
              <div className="students img-container" onClick={() => {}}>
-                 <img src="../../static/assets/LandingPage/Apply Now.png" />
-                 <p>Apply Now!</p>
+               <img src="../../static/assets/LandingPage/Apply Now.png" />
+               <p>Apply Now!</p>
              </div>
-             
+
              <div className="donor img-container" onClick={() => {}}>
-                <img src="../../static/assets/LandingPage/Donate Now.png" />
-                <p className="donate-text">Donate Now!</p>
+               <img src="../../static/assets/LandingPage/Donate Now.png" />
+               <p className="donate-text">Donate Now!</p>
              </div>
 
              <div className="employers img-container" onClick={() => {}}>
-                 <img src="../../static/assets/LandingPage/Search Now.png" />
-                 <p>Search Now!</p>
+               <img src="../../static/assets/LandingPage/Search Now.png" />
+               <p>Search Now!</p>
              </div>
-
            </div>
          </section>
 
@@ -79,16 +90,21 @@
                  alt="Debt man"
                />
              </div>
+             <div classname="zero-tuition-cta">
              <h2>OUR PURPOSE</h2>
              <p className="title">
                Training next level professionals to
                <span className="highlighted-text">empower communities</span>.
              </p>
+             </div>
            </div>
          </section>
 
-         <section className="on-th-job-training">
-           <img src="../../static/assets/LandingPage/You got this bud.png" alt="two buds" />
+         <section className="on-the-job-training">
+           <img
+             src="../../static/assets/LandingPage/You got this bud.png"
+             alt="two buds"
+           />
            <div className="training-cta">
              <p>On the Job Training</p>
              <button>Let's Do This!</button>
@@ -98,41 +114,55 @@
          <section className="full-scale-applicatoin">
            <div className="full-scale-applicatoin-cta">
              <h2>Full Scale Application</h2>
-             <p>Log classroom and on-the-job-training hours all under one streamlined application</p>
+             <p>
+               Log classroom and on-the-job-training hours all under one
+               streamlined application
+             </p>
              <button>Apply Now!</button>
            </div>
-           <img src="../../static/assets/LandingPage/Classroom Training.png" alt="two people in a classroom"/>
+           <img
+             src="../../static/assets/LandingPage/Classroom Training.png"
+             alt="two people in a classroom"
+           />
          </section>
 
          <section className="featured-project">
-             
            <div className="title-and-img user-story">
-             <img src="" alt="" />
+             <img
+               src="../../static/assets/LandingPage/Rubicon phones.png"
+               alt="cell phone"
+             />
            </div>
 
            <div className="user-story">
-             <img src="" alt="" />
-             <h2 className="user-info">
-
-             </h2>
-             <p>
-                 
-             </p>
+             <img
+               src="../../static/assets/LandingPage/Team Rubicon.png"
+               alt="construction worker"
+             />
+             <h2 className="user-info">Team Rubicon - Flint, MI</h2>
+             <p>Come see the journey of our students and industry experts restore an abandoned school building to a modern state-of-the-art career school.  The new school year will be starting Fall of 2020.  Thank you to our amazing donors and partners for inspiring our apprentices.</p>
            </div>
-           <img src="" alt=""/>
+           <img
+             src="../../static/assets/LandingPage/Rubicon phones.png"
+             alt="cell phone"
+           />
          </section>
 
          <section className="search-projects-near-you">
+           <form onSubmit={() => {}} className="search-projects">
+             <h2>Search for Apprenticeship Projects Near You!</h2>
+             <p className="description">Explore various projects trending in your local area and be the first to see what's going on.</p>
+             <input type="text" placeholder="Enter Location" />
+             <button>Search Now!</button>
+           </form>
 
-             <form onSubmit={()=>{}} className="search-projects">
-                 <p className="description">
-                 </p>
-                 <input type="text" placeholder="Enter Location"/>
-                 <button></button>
-             </form>
-
-             {/* This is where the map will go from our 3rd party solution */}
+           {/* This is where the map will go from our 3rd party solution */}
          </section>
+
+         <section className='testimonials'>
+
+         </section>
+
          {/* <Footer /> */}
        </>
      );
