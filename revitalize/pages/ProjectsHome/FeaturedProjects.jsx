0ac6c8d4d0e01a3df14e9a470eb40510fcc2f4b4
Zealll <eriznis@gmail.com>
2019-09-13 14:49:16 -0400

    :lipstick: Featured projects created

--- revitalize/pages/ProjectsHome/FeaturedProjects.jsx
+++ revitalize/pages/ProjectsHome/FeaturedProjects.jsx
@@ -2,34 +2,34 @@ import React, {useState} from 'react';
 
 const projects = [
     {
-        title: 'Project 1',
-        description: 'description 1',
-        img: 'http://cupheadmemes.com/wp-content/uploads/2018/08/Best-Programming-Memes-026.jpg'
+        title: 'Team Rubicon',
+        description: 'Learn next to master trade professionals that are available during the day and after hours. Learn next to master trade Learn next to master trade professionals that are available during the day and after hours. Learn next to master trade',
+        img: '../static/assets/ProjectPage/TR.png'
     },
     {
-        title: 'Project 2',
-        description: 'description 2',
-        img: 'http://cupheadmemes.com/wp-content/uploads/2018/08/Best-Programming-Memes-026.jpg'
+        title: 'Heavy Metal Queen',
+        description: 'Learn next to master trade professionals that are available during the day and after hours.',
+        img: '../static/assets/ProjectPage/HMQ.png'
     },
     {
-        title: 'Project 3',
-        description: 'description 3',
-        img: 'http://cupheadmemes.com/wp-content/uploads/2018/08/Best-Programming-Memes-026.jpg'
+        title: 'Jean Childs Project',
+        description: 'Learn next to master trade professionals that are available during the day and after hours.',
+        img: '../static/assets/ProjectPage/JPP.png'
     },
     {
-        title: 'Project 4',
-        description: 'description 4',
-        img: 'http://cupheadmemes.com/wp-content/uploads/2018/08/Best-Programming-Memes-026.jpg'
+        title: 'Mission One Project',
+        description: 'Learn next to master trade professionals that are available during the day and after hours.',
+        img: '../static/assets/ProjectPage/M1p.png'
     },
     {
-        title: 'Project 5',
-        description: 'description 5',
-        img: 'http://cupheadmemes.com/wp-content/uploads/2018/08/Best-Programming-Memes-026.jpg'
+        title: 'Guidr Project',
+        description: 'Learn next to master trade professionals that are available during the day and after hours.',
+        img: '../static/assets/ProjectPage/GP.png'
     },
     {
-        title: 'Project 6',
-        description: 'description 6',
-        img: 'http://cupheadmemes.com/wp-content/uploads/2018/08/Best-Programming-Memes-026.jpg'
+        title: 'Clover 4',
+        description: 'Learn next to master trade professionals that are available during the day and after hours.',
+        img: '../static/assets/ProjectPage/Clover4.png'
     },
     {
         title: 'Project 7',
@@ -49,18 +49,27 @@ const FeaturedProjects = () => {
     if(!projects) return <div>Loading</div>
     return (
         <div className='FPContainer'>
+            <h1>Featured Projects</h1>
+            <div className='FPProjectList'>
             {featured.map((each, i) => {
                 if(i < 6){
                     return (
                     <div className='FPProjects' key={i}>
                         <img src={each.img} />
                         <h4>{each.title}</h4>
-                        <p>{each.description}</p>
+                        {console.log(each.description.length)}
+                        <p>{each.description.split('').map((eachLetter, letterLength) => {
+                                if(letterLength <= 118){
+                                    return eachLetter
+                                }
+                            }).join('')}{each.description.length > 118 ? "..." : null}
+                        </p>
                     </div>
                     )
                 }
                 
             })}
+            </div>
         </div>
     );
 };
