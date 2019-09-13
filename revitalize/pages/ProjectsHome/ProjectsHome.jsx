8148eada48b1c208f4fa827dc436f42a8610c58f
Clark Williams <theclarkwilliams@gmail.com>
2019-09-12 18:04:31 -0600

    :art: start xml sturcture
    started jsx and beginning of scss

--- revitalize/pages/ProjectsHome/ProjectsHome.jsx
+++ revitalize/pages/ProjectsHome/ProjectsHome.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 
-const FeaturedProjects = () => {
+const ProjectsHome = () => {
     return (
-        <div>
+        <div className="projects-home">
             {/* top */}
+            <section className="top">
+
+            </section>
             {/* featured trades */}
-            {/* curvy part */}
+            <section className="ft-container">
+                <h3 className="ft-title">Featured Trades</h3>
+                <p>Learn next to master trade professionals that are available during the day and after hours.</p>
+                <p href="">Show More Trades &rsaquo;</p>
+                <div className="featured-trades">
+                    <div className="ft-box">
+                        <img src="" />
+                        <p>Electronics</p>
+                    </div>
+                    <div className="ft-box">
+                        <img src="" />
+                        <p>Construction</p>
+                    </div>
+                    <div className="ft-box">
+                        <img src="" />
+                        <p>Mechanics</p>
+                    </div>
+                    <div className="ft-box">
+                        <img src="" />
+                        <p>Welding</p>
+                    </div>
+                </div>
+            </section>
+            {/* curvy part/thing */}
             {/* featured projects */}
 
             {/* top trending trades */}
@@ -15,4 +41,4 @@ const FeaturedProjects = () => {
     );
 };
 
-export default FeaturedProjects;
+export default ProjectsHome;
--- revitalize/pages/index.js
+++ revitalize/pages/index.js
@@ -3,13 +3,16 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Nav from '../components/Layout/Nav';
 import LandingPage from '../pages/Landing/LandingPage';
+import ProjectsHome from '../pages/ProjectsHome/ProjectsHome';
+
 import '../styles/base.scss';
 
 const Home = () => (
 	<div>
 		<Head></Head>
 		<Nav />
-		<LandingPage />
+		{/* <LandingPage /> */}
+		<ProjectsHome />
 	</div>
 );
 
