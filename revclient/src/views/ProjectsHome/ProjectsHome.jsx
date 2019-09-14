296cb7546fbd193759a956448e3ebe62666164c5
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-09-13 22:14:54 -0700

    :sparkles: jGit

--- revclient/src/views/ProjectsHome/ProjectsHome.jsx
+++ revclient/src/views/ProjectsHome/ProjectsHome.jsx
@@ -3,15 +3,16 @@ import FeaturedProjects from './FeaturedProjects.jsx';
 import TopSection from './TopSection.jsx';
 import FeaturedTrades from './FeaturedTrades.jsx';
 import CurriculumOptions from './CurriculumOptions.jsx';
-
+import StudentCTA from "./StudentCTA/StudentCTA"
 
 const ProjectsHome = () => {
 	return (
 		<div className="projects-home">
 			<TopSection />
-            <FeaturedTrades />
-            <CurriculumOptions />
+			<FeaturedTrades />
+			<CurriculumOptions />
 			<FeaturedProjects />
+			<StudentCTA />
 		</div>
 	);
 };
