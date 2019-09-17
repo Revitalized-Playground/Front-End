fc0a998d09afba87270f44d26b25feed1f682c26
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-09-17 12:04:49 -0700

    Initial Project Page Commit. Infrasctructure for styling and route set to begin work on the single project page

---
+++ revclient/src/views/ProjectPage/ProjectPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectPage = () => {
+    return (
+        <div className="project-page-container">
+            Project Page
+        </div>
+    );
+};
+
+export default ProjectPage;
