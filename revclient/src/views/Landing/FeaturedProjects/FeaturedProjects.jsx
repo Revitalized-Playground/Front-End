a2f7733b28139b65aaafd2a160edf2e89def0041
Clark Williams <theclarkwilliams@gmail.com>
2019-09-17 16:50:45 -0600

    FeaturedProject styling aligned, need to go back and make some units more responsive

--- revclient/src/views/Landing/FeaturedProjects/FeaturedProjects.jsx
+++ revclient/src/views/Landing/FeaturedProjects/FeaturedProjects.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import rubiconPhones from "../../../assets/LandingPage/RubiconPhones.png";
 import teamRubicon from "../../../assets/LandingPage/Team Rubicon.png";
 
 const FeaturedProjects = () => {
-    return (
-        <section className="featured-project">
-        <img
-          className="phone-left phone"
-          src={rubiconPhones}
-          alt="cell phone"
-        />
+  
+  const initStory = "Come see the journey of our students and industry experts restore an abandoned school building to a modern state-of-the-art career school. The new school year will be starting Fall of 2020. Thank you to our amazing donors and partners for inspiring our apprentices."
+  
+  const [projectTitle, setProjectTitle] = useState("Team Rubicon - Flint, MI")
+  const [projectStory, setProjectStory] = useState(initStory);
 
-        <div className="user-story">
-          <img
-            className="user-photo"
-            src={teamRubicon}
-            alt="construction worker"
-          />
-          <h2 className="user-info">Team Rubicon - Flint, MI</h2>
-          <p>
-            Come see the journey of our students and industry experts restore an
-            abandoned school building to a modern state-of-the-art career
-            school. The new school year will be starting Fall of 2020. Thank you
-            to our amazing donors and partners for inspiring our apprentices.
-          </p>
-        </div>
+  // still need to make images dynamic
+
+  return (
+    <section className="featured-project">
+      <h2 className="section-title">Featured Project</h2>
+      <img
+        className="phone-left phone"
+        src={rubiconPhones}
+        alt="cell phone"
+      />
+      <div className="user-story">
         <img
-          className="phone-right phone"
-          src={rubiconPhones}
-          alt="cell phone"
+          className="user-photo"
+          src={teamRubicon}
+          alt="construction worker"
         />
-      </section>
+        <h2 className="user-info">{projectTitle}</h2>
+        <p>{projectStory}</p>
+      </div>
+      <img
+        className="phone-right phone"
+        src={rubiconPhones}
+        alt="cell phone"
+      />
+    </section>
     );
 };
 
