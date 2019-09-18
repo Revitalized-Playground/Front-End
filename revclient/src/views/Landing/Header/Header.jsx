9cd1e7f7d8e4b188df91186a03332185294aea05
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-09-18 15:38:35 -0700

    Minor adjustments

--- revclient/src/views/Landing/Header/Header.jsx
+++ revclient/src/views/Landing/Header/Header.jsx
@@ -6,30 +6,12 @@ import donateNow from '../../../assets/LandingPage/Donate Now.png';
 import searchNow from '../../../assets/LandingPage/Search Now.png';
 
 const Header = () => {
-	
+
 	const [sHovered, setSHovered] = useState(false);
 	const [dHovered, setDHovered] = useState(false);
 	const [eHovered, setEHovered] = useState(false);
-	
-	
-	// const [hovered, setHovered] = useState(false);
-	// const [hovered, setHovered] = useState({
-	// 	students: false,
-	// 	donor: false,
-	// 	employers: false
-	// });
 
-	// const toggleHover = x => {
-	// 	// e.preventDefault();
-	// 	// let a
-	// 	// console.log(e.target.alt);
-	// 	// e.target.alt === "Apply Now!"
-	// 	// 	? 
-	// 	setHovered({
-	// 		...hovered,
-	// 		x: !x
-	// 	});
-	// }
+
 
 
 
@@ -73,18 +55,18 @@ const Header = () => {
 			</div>
 
 			<div className="application-links-container">
-				<div 
+				<div
 					className="students img-container"
-					onClick={() => {}}
+					onClick={() => { }}
 					onMouseOver={() => setSHovered(!sHovered)}
 					onMouseOut={() => setSHovered(!sHovered)}
-					// onMouseOver={e => e.target.value = "Learn next to world-class professionals"}
-					// onMouseOver={() => toggleHover(hovered.students)}
-					// onMouseOut={() => toggleHover(hovered.students)}
+				// onMouseOver={e => e.target.value = "Learn next to world-class professionals"}
+				// onMouseOver={() => toggleHover(hovered.students)}
+				// onMouseOut={() => toggleHover(hovered.students)}
 				>
 					<img src={applyNow} alt="Apply Now!" />
 					{!sHovered
-					// { !hovered.students
+						// { !hovered.students
 						? <p>Apply Now!</p>
 						: <p className="hovered">Learn next to world-class professionals</p>
 						// : <p className={hovered ? "hovered" : ""}>Learn next to world-class professionals</p>
@@ -94,9 +76,9 @@ const Header = () => {
 					}
 				</div>
 
-				<div 
+				<div
 					className="donor img-container"
-					onClick={() => {}}
+					onClick={() => { }}
 					// onMouseOver={e => toggleHover(e)}
 					// onMouseOut={e => toggleHover(e)}
 					onMouseOver={() => setDHovered(!dHovered)}
@@ -109,9 +91,9 @@ const Header = () => {
 					}
 				</div>
 
-				<div 
+				<div
 					className="employers img-container"
-					onClick={() => {}}
+					onClick={() => { }}
 					// onMouseOver={e => toggleHover(e)}
 					// onMouseOut={e => toggleHover(e)}
 					onMouseOver={() => setEHovered(!eHovered)}
@@ -120,9 +102,9 @@ const Header = () => {
 					<img src={searchNow} alt="Search now" />
 					{!eHovered
 						? <p>Search Now!</p>
-						: <p className="hovered">Hire qualified professionals hungry to make an impact</p>
+						: <p className="hovered">Hire qualified professionals hungry to<br /> make an impact</p>
 					}
-					
+
 				</div>
 			</div>
 		</section>
