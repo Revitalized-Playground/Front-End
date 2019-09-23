3a88a3135117bef64d84a0886e439edfaa7ce6d6
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-09-23 11:47:49 -0700

    Added route for create Project wizard

--- revclient/src/Routes.js
+++ revclient/src/Routes.js
@@ -1,62 +1,37 @@
-import React from 'react'
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 // Routes
-import LandingPage from "./views/Landing/LandingPage";
-import Register from "./views/Register/Register";
-import Login from "./views/Login/Login";
-import ProjectsHome from "./views/ProjectsHome/ProjectsHome"; 
+import LandingPage from './views/Landing/LandingPage';
+import Register from './views/Register/Register';
+import Login from './views/Login/Login';
+import ProjectsHome from './views/ProjectsHome/ProjectsHome';
+import CreateProjWizard from './views/CreateProjWizard/CreateProjWozard';
 
 // Utils
-import AuthenticateUser from "./utils/AuthenticateUser";
+import AuthenticateUser from './utils/AuthenticateUser';
 
 export const Routes = props => {
+	return (
+		<BrowserRouter>
+			{/* public routes */}
 
-    return (
-        <BrowserRouter>
-            
-            {/* public routes */}
-            
-            <Route
-                exact
-                path="/"
-                render={() => (
-                    <LandingPage />
-                )}
-		    />
-            <Route
-                exact
-                path="/register"
-                render={() => (
-                    <Register />
-                )}
-            />
-            <Route
-                exact
-                path="/login"
-                render={() => (
-                    <Login />
-                )}
-            />
-            <Route 
-                exact
-                path="/oauth/:token"
-                component={AuthenticateUser}
-		    />
-            {/* private routes */}
+			<Route exact path="/" render={() => <LandingPage />} />
+			<Route exact path="/register" render={() => <Register />} />
+			<Route exact path="/login" render={() => <Login />} />
+			<Route exact path="/oauth/:token" component={AuthenticateUser} />
+			{/* private routes */}
 
-            <Route
-                // exact
-                path="/projects"
-                render={() => (
-                    <ProjectsHome />
-                )}
-            />
-
-
-        </BrowserRouter>
-    );
-}
-
-
-
+			<Route
+				// exact
+				path="/projects"
+				render={() => <ProjectsHome />}
+			/>
+			<Route
+				// exact
+				path="/createproject"
+				render={() => <CreateProjWizard />}
+			/>
+		</BrowserRouter>
+	);
+};
