62cc82bd4ec47f8d6d173c076105a9c7905638a7
Frank Martinez <frankie.martinez@gmail.com>
2019-09-24 21:35:45 -0700

    🎨 Minor styling

---
+++ revclient/src/test.js
@@ -0,0 +1,20 @@
+const gunArray = [
+    { id: 1, gunName: "SMG" },
+    { id: 2, gunName: "AK47" },
+    { id: 3, gunName: "9M" },
+    { id: 4, gunName: "" },
+    { id: 5, gunName: "" },
+    { id: 6, gunName: "" },
+
+]
+
+
+
+    gunArray.map(gun => {
+        if( !name.gunName) {
+            return null
+        }
+        <div>{gun.gunName}</div>
+    })
+
+
