e998483e0eca81f6503cbb6ae530a5693b14c2ee
Jose Montero <josemariodev@gmail.com>
2019-09-26 17:09:43 -0500

    styling updates to hire component

--- revclient/src/views/Start/Hire/Company/Company.jsx
+++ revclient/src/views/Start/Hire/Company/Company.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Company = props => {
+	const { image, paintbrush, logo } = props.info;
 	return (
-		<div className="companyContainer" style={{ backgroundImage: `url(${props.info.image})` }}>
+		<div className="companyContainer" style={{ backgroundImage: `url(${image})` }}>
 			<div className="logoContainer">
-				<img className="logo" src={props.info.logo} alt="logo" />
-				<img src={props.info.paintbrush} alt="paintbrush" />
+				<img className="logo" src={logo} alt="logo" />
+				<img src={paintbrush} alt="paintbrush" />
 			</div>
 		</div>
 	);
