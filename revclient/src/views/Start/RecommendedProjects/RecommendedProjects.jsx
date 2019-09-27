5f8953f0263b4645476f43547ab54dd1ee9ca25f
Frank Martinez <frankie.martinez@gmail.com>
2019-09-27 12:26:05 -0700

    📱 Cleaning up noteworthy

--- revclient/src/views/Start/RecommendedProjects/RecommendedProjects.jsx
+++ revclient/src/views/Start/RecommendedProjects/RecommendedProjects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Slider from "react-slick";
 
-import CarouselCard from '../Projects/ProjectsCarousel/CarouselCard';
+import CarouselCard from '../CarouselCard/CarouselCard';
 
 
 class RecommendedProjects extends React.Component {
