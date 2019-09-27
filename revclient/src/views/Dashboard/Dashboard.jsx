e64de9cd9e5dcfb3996559d5efc16dd623716418
Jose Montero <josemariodev@gmail.com>
2019-09-27 17:40:05 -0500

    merge w/staging

--- revclient/src/views/Dashboard/Dashboard.jsx
+++ revclient/src/views/Dashboard/Dashboard.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
-<<<<<<< HEAD
+
 import Nav from '../../components/Layout/Nav';
-=======
-import Nav from "../../components/Layout/Nav";
->>>>>>> 937e75dae43d688becd3cd19c7ec70126c22c129
+
 import UserDashboard from "./User/UserDashboard";
 import Footer from "../../components/Layout/Footer";
 
