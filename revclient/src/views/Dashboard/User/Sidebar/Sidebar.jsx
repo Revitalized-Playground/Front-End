b7fcffd627b1f75de6d49d03589e7ca87222e313
Clark Williams <theclarkwilliams@gmail.com>
2019-09-30 12:16:21 -0600

    Add most of JSX, should be mostly ready for styling

--- revclient/src/views/Dashboard/User/Sidebar/Sidebar.jsx
+++ revclient/src/views/Dashboard/User/Sidebar/Sidebar.jsx
@@ -2,34 +2,115 @@ import React from 'react';
 import Skeleton,  { SkeletonTheme } from "react-loading-skeleton";
 
 const Sidebar = props => {
+    
+
+    console.log(props.user);
+
     return (
         <section className="user-dashboard-sidebar section">
             {
-                // <img src={props.user.profileImage} alt="user" />
-                // ||
+                <img src={props.user.profileImage} alt="user" />
+                ||
                 <Skeleton circle={true} height={110} width={110} />
             }
-
-            <Skeleton count={2} />
-            <div className="user-dashboard-title">
-                    <Skeleton count={1} height={25} width={200} />
-                    <Skeleton count={1} height={25} width={125} />
-                    <Skeleton count={1} height={25} width={75} /> 
+            {
+                <>
+                    <h3>{`${props.user.firstName} ${props.user.lastName}`}</h3>
+                    <p>{props.user.handle}</p>
+                </>
+                ||
+                <Skeleton count={2} />
+            }
+            <div className="user-dashboard-stats">
+                {
+                    <>
+                        <div className="quick-stat">
+                            <h4>{props.user.certifications}</h4>
+                            <p>Certifications</p>
+                        </div>
+                        <div className="quick-stat">
+                            <h4>{props.user.hoursLogged}</h4>
+                            <p>Hours Logged</p>
+                        </div>
+                        <div className="quick-stat">
+                            <h4>{props.user.creditHours}</h4>
+                            <p>Credit Hours</p>
+                        </div>
+                    </>
+                    ||
+                    <>
+                        <Skeleton count={1} height={25} width={200} />
+                        <Skeleton count={1} height={25} width={125} />
+                        <Skeleton count={1} height={25} width={75} />
+                    </>
+                }
             </div>
-
             <hr/>
-
-            <SkeletonTheme color="#ffffff"  >
-                <Skeleton count={15} duration={100} />
-            </SkeletonTheme>
-
+            <div className="info-container">
+                {
+                    <>
+                        <div className="progress">
+                            <p>Overall progress of project</p>
+                            <span>progress bar here</span>
+                        </div>
+                        <div className="info">
+                            <div className="text">
+                                <p>Zoom</p>
+                                <span>{props.user.zoom}</span>
+                            </div>
+                            <div className="icon">zoom icon</div>
+                        </div>
+                        <div className="info">
+                            <div className="text">
+                                <p>Email</p>
+                                <span>{props.user.email}</span>
+                            </div>
+                            <div className="icon">email icon</div>
+                        </div>
+                        <div className="info">
+                            <div className="text">
+                                <p>Phone Number</p>
+                                <span>{props.user.phoneNumber}</span>
+                            </div>
+                            <div className="icon">phone icon</div>
+                        </div>
+                        <div className="info">
+                            <div className="text">
+                                <p>Location</p>
+                                <span>{`${props.user.city}, ${props.user.state}`}</span>
+                            </div>
+                            <div className="icon">location icon</div>
+                        </div>
+                    </>
+                    ||
+                    <SkeletonTheme>
+                        <Skeleton count={5} duration={100} />
+                    </SkeletonTheme>
+                }
+            </div>
             <hr/>
-
             <div className="user-dashboard-sidebar-footer">
-                <Skeleton circle={true} height={63} width={63} />
-                <Skeleton circle={true} height={63} width={63} />
-                <Skeleton circle={true} height={63} width={63} />
-                <Skeleton circle={true} height={63} width={63} />
+                <h4>Achievements</h4>
+                {
+                    props.user.achievements.map(a => (
+                        <div className="achievement">
+                            <img src={a.image} alt={`${a.name} achievement`} className="a-icon"/>
+                            <div className="number">
+                                { a.number < 31
+                                    ? `${a.number}`
+                                    : `30+`
+                                }
+                            </div>
+                        </div>
+                    ))
+                    ||
+                    <>
+                        <Skeleton circle={true} height={63} width={63} />
+                        <Skeleton circle={true} height={63} width={63} />
+                        <Skeleton circle={true} height={63} width={63} />
+                        <Skeleton circle={true} height={63} width={63} />
+                    </>
+                }
             </div>
         </section>
     );
--- revclient/src/views/Dashboard/User/UserDashboard.jsx
+++ revclient/src/views/Dashboard/User/UserDashboard.jsx
@@ -3,52 +3,76 @@ import Skeleton,  { } from "react-loading-skeleton";
 
 import Sidebar from './Sidebar/Sidebar';
 
+// import { useQuery } from '@apollo/react-hooks';
+// import { GET_USER } from '../../graphql/queries/Users';
+
 const UserDashboard = () => {
+
+    // const { client, loading, error, data } = useQuery(GET_USER);
 
     const users = [
         {
-            skyelar: {
-                id: 1,
-                userAccountId: 1,
-                email: "fractured2k@gmail.com",
-                firstName: "Skyelar",
-                lastName: "Carrol",
-                profileImage: "",
-                city: "Grand Haven",
-                zip: "",
-                address: "",
-                state: "MI",
-                aptNumber: "",
-                projects: [],
-                phoneNumber: "889-214-8971",
-                zoom: "SkyfiCrole",
-                certifications: 2,
-                hoursLogged: 396,
-                creditHours: 22,
-                achievements: [],
-            }
+            id: 1,
+            userAccountId: 1,
+            email: "fractured2k@gmail.com",
+            firstName: "Skyelar",
+            lastName: "Carrol",
+            handle: "@Skyelar",
+            profileImage: "",
+            city: "Grand Haven",
+            zip: "",
+            address: "",
+            state: "MI",
+            aptNumber: "",
+            projects: [],
+            phoneNumber: "889-214-8971",
+            zoom: "SkyfiCrole",
+            certifications: 2,
+            hoursLogged: 396,
+            creditHours: 22,
+            achievements: [
+                {
+                    name: "Master Key Award",
+                    image: "",
+                    number: 2
+                },
+                {
+                    name: "Thanos Grip Award",
+                    image: "",
+                    number: 1
+                },
+                {
+                    name: "Magic Wand",
+                    image: "",
+                    number: 12
+                },
+                {
+                    name: "Rupee Award",
+                    image: "",
+                    number: 31
+                }
+            ],
         },
         {
-            maleescha: {
-                id: 2,
-                userAccountId: 2,
-                email: "Maleescha Mclarin@gmail.com",
-                firstName: "Maleescha",
-                lastName: "Mclarin",
-                profileImage: "",
-                city: "Detroit",
-                zip: "",
-                address: "",
-                state: "MI",
-                aptNumber: "",
-                projects: [],
-                phoneNumber: "356-230-9971",
-                zoom: "TheeMclarin",
-                certifications: 2,
-                hoursLogged: 396,            
-                creditHours: 22,
-                achievements: [],
-            }
+            id: 2,
+            userAccountId: 2,
+            email: "Maleescha Mclarin@gmail.com",
+            firstName: "Maleescha",
+            lastName: "Mclarin",
+            handle: "@TheeMclarin ",
+            profileImage: "",
+            city: "Detroit",
+            zip: "",
+            address: "",
+            state: "MI",
+            aptNumber: "",
+            projects: [],
+            phoneNumber: "356-230-9971",
+            zoom: "TheeMclarin",
+            certifications: 2,
+            hoursLogged: 396,            
+            creditHours: 22,
+            achievements: [],
         }
     ]
 
@@ -56,15 +80,12 @@ const UserDashboard = () => {
         // setApprentice
     ] = useState(true);
 
-    // const toggleUserType = () => {
-    //     setApprentice(!apprentice)
-    // }
     return (
         <>
             <section className="user-dashboard">
                 { apprentice
-                    ? <Sidebar user={users.skyelar} />
-                    : <Sidebar user={users.maleescha} />
+                    ? <Sidebar user={users[0]} />
+                    : <Sidebar user={users[1]} />
                 }
                 
                 <section className="user-dashboard-body">
