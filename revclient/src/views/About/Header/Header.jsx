69757d8a51423b7e6b930dcae7d78fe118c3a394
Clark Williams <theclarkwilliams@gmail.com>
2019-10-02 11:32:05 -0600

    started styling about-header

--- revclient/src/views/About/Header/Header.jsx
+++ revclient/src/views/About/Header/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="about-header">
+      {/* <div className="images">
+        <img src="https://res.cloudinary.com/revitalize/image/upload/v1570037035/about%20page/Hero_Images1_k5qnxo.svg" alt="" />
+        <img src="https://res.cloudinary.com/revitalize/image/upload/v1570036574/about%20page/Hero_Images_2_cuklhr.svg" alt="" />
+      </div>
+      <h1>Who We Are</h1> */}
+      <img src="https://res.cloudinary.com/revitalize/image/upload/v1570037035/about%20page/Hero_Images1_k5qnxo.svg" alt="" />
+      <h1>Who We Are</h1>
+      <img src="https://res.cloudinary.com/revitalize/image/upload/v1570036574/about%20page/Hero_Images_2_cuklhr.svg" alt="" />
     </div>
   )
 }
