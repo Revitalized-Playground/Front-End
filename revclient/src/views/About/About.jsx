82b656664cb6874beadda3267710a253991d3a4f
Jose Montero <josemariodev@gmail.com>
2019-10-02 11:36:08 -0500

    add nav and footer to About

--- revclient/src/views/About/About.jsx
+++ revclient/src/views/About/About.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import Header from '../Header/Header';
-import Main from '../Main/Main';
-import Squad from '../Squad/Squad';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Squad from './Squad/Squad';
+import Nav from '../../components/Layout/Nav';
+import Footer from '../../components/Layout/Footer';
 
 const About = () => {
 	return (
 		<div>
+			<Nav />
 			<Header />
 			<Main />
 			<Squad />
+			<Footer />
 		</div>
 	);
 };
