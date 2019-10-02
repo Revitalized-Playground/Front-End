994d2df39f3a0d4b49e7d0a0a94730b5c8d58cb4
Frank Martinez <frankie.martinez@gmail.com>
2019-10-02 13:08:25 -0700

    📱 Login mobile style
    Removed picture for mobile. Pulled in cloudinary link for faster loading. Restructured component for easier readability.

--- revclient/src/views/Login/Login.jsx
+++ revclient/src/views/Login/Login.jsx
@@ -8,10 +8,10 @@ import {
 
 import { Link, withRouter } from "react-router-dom";
 
-import revitalizeLogo from "../../assets/LandingPage/Logo.png";
 import googleLogo from "../../assets/AuthPages/Google.png";
 import fbLogo from "../../assets/AuthPages/fb-logo.png";
-// import twitterLogo from "../../assets/AuthPages/twitter.png";
+
+import Nav from "../../components/Layout/Nav";
 
 
 const Login = props => {
@@ -43,73 +43,76 @@ const Login = props => {
 	};
 	
 	return (
-		<div className="login-container">
-			<div className="login-container-left">
-				<div className="revitalizeLogo-container">
-					<Link to="/" title="Home">
-						<div>
-							<img src={revitalizeLogo} alt="Revitalize logo" />
-						</div>
-					</Link>
-				</div>
-				<div className="login-form">
-					<div className="login-third-party">
-						<div className="login-welcome">
-							<h2>Welcome Back!</h2>
-						</div>
-						<button>
+		<>
+			<Nav page="login" />
+			<div className="login-container">
+				<div className="login-container-left">
+					{/* <div className="revitalizeLogo-container">
+						<Link to="/" title="Home">
 							<div>
-								<a className="login-button" href={`${process.env.REACT_APP_OAUTH_GOOGLE_LINK}`}>
-									<img src={googleLogo} alt="Google logo" />
-									<h5>Sign In With Google</h5>
-								</a>
+								<img src={revitalizeLogo} alt="Revitalize logo" />
 							</div>
-						</button>
-						<button>
-							<div>
-								<a className="login-button" href={`${process.env.REACT_APP_OAUTH_FACEBOOK_LINK}`}>
-									<img src={fbLogo} alt="Facebook logo" />
-									<h5>Sign In With Facebook</h5>
-								</a>
+						</Link>
+					</div> */}
+					<div className="login-form">
+						<div className="login-third-party">
+							<div className="login-welcome">
+								<h2>Welcome Back!</h2>
 							</div>
-						</button>
-					</div>
-					<div className="login-middle">
-						<div className="login-line"></div>
-						<p>or</p>
-						<div className="login-line"></div>
-					</div>
-					<form className="login-local" onSubmit={handleSubmit}>
-						<p className="login-title">Email</p>
-						<input
-							name='email'
-							type='email'
-							placeholder="Email..."
-							value={state.email}
-							onChange={handleChanges}
-                		/>
-						<div className="login-pass">
-							<p className="">Password</p>
-							<span className="">Forgot Password?</span>
+							<button>
+								<div>
+									<a className="login-button" href={`${process.env.REACT_APP_OAUTH_GOOGLE_LINK}`}>
+										<img src={googleLogo} alt="Google logo" />
+										<h5>Sign In With Google</h5>
+									</a>
+								</div>
+							</button>
+							<button>
+								<div>
+									<a className="login-button" href={`${process.env.REACT_APP_OAUTH_FACEBOOK_LINK}`}>
+										<img src={fbLogo} alt="Facebook logo" />
+										<h5>Sign In With Facebook</h5>
+									</a>
+								</div>
+							</button>
 						</div>
-						<input
-							name="password"
-							type="password"
-							placeholder="Password..."
-							value={state.password}
-							onChange={handleChanges}
-                		/>
-						<div className="login-mid">
-							<p>
-								Don't have an account? <Link to='/register' style={{textDecoration: `underline`}}>Create One</Link>
-							</p>
+						<div className="login-middle">
+							<div className="login-line"></div>
+							<p>or</p>
+							<div className="login-line"></div>
 						</div>
-						<button>Log In</button>
-					</form>
+						<form className="login-local" onSubmit={handleSubmit}>
+							<p className="login-title">Email</p>
+							<input
+								name='email'
+								type='email'
+								placeholder="Email..."
+								value={state.email}
+								onChange={handleChanges}
+							/>
+							<div className="login-pass">
+								<p className="">Password</p>
+								<span className="">Forgot Password?</span>
+							</div>
+							<input
+								name="password"
+								type="password"
+								placeholder="Password..."
+								value={state.password}
+								onChange={handleChanges}
+							/>
+							<div className="login-mid">
+								<p>
+									Don't have an account? <Link to='/register' style={{textDecoration: `underline`}}>Create One</Link>
+								</p>
+							</div>
+							<button>Log In</button>
+						</form>
+					</div>
 				</div>
+				<div className="imgContainer"></div>
 			</div>
-			<div className="imgContainer"></div>
-		</div>
+		</>
 	);
 }
 
