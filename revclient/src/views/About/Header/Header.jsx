3540daf46d0b3291131f0009f49c3fff51d96e98
Clark Williams <theclarkwilliams@gmail.com>
2019-10-03 11:21:09 -0600

    fixed dropdown z-index again, fixed hero2 image in about header

--- revclient/src/views/About/Header/Header.jsx
+++ revclient/src/views/About/Header/Header.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 
+import Hero2 from '../../../assets/AboutPage/Hero2.png';
+
 const Header = () => {
   return (
     <>
       <div className="about-header">
-        {/* <h1>Who We Are</h1>
-        <div className="images">
-          <img src="https://res.cloudinary.com/revitalize/image/upload/v1570037035/about%20page/Hero_Images1_k5qnxo.svg" alt="" />
-          <img src="https://res.cloudinary.com/revitalize/image/upload/v1570036574/about%20page/Hero_Images_2_cuklhr.svg" alt="" />
-        </div> */}
         <img
           src="https://res.cloudinary.com/revitalize/image/upload/v1570037035/about%20page/Hero_Images1_k5qnxo.svg"
           alt="" />
         <h1>Who We Are</h1>
         <img
-          src="https://res.cloudinary.com/revitalize/image/upload/v1570036574/about%20page/Hero_Images_2_cuklhr.svg"
+          src={Hero2}
           alt=""
           className="img2" />
       </div>
