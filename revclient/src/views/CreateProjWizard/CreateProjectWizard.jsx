0a007f69830aa39c175b89c0d691d9258d4cd472
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-02 18:32:24 -0700

    Made project photos dynamic
    Utilized project query to dynamically render photos

--- revclient/src/views/CreateProjWizard/CreateProjectWizard.jsx
+++ revclient/src/views/CreateProjWizard/CreateProjectWizard.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
-import moment from "moment";
+import moment from 'moment';
 
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_PROJECT } from '../../graphql/mutations/Project';
 
-import Nav from "../../components/Layout/Nav";
+import Nav from '../../components/Layout/Nav';
 import Form1 from './Form1/Form1';
 import Form2 from './Form2/Form2';
 import Form3 from './Form3/Form3';
@@ -16,111 +16,114 @@ import Form3 from './Form3/Form3';
 //  - estimated project projDuration
 //  - difficulty Level
 
+let currentDate = moment().format('YYYY-MM-DD');
 
+const CreateProjectWizard = ({ history }) => {
+	const [projectDetails, setProjectDetails] = useState({
+		name: '',
+		startDate: currentDate,
+		country: 'USA',
+		duration: 1,
+		description: '',
+		address: '',
+		city: '',
+		state: '',
+		zip: null,
+		goalAmount: 0.0,
+		amountFunded: 0.0,
+		difficulty: '',
+		images: [],
+	});
+	const [formPosition, setFormPosition] = useState(1);
+	const [addProject] = useMutation(ADD_PROJECT);
 
-let currentDate = moment().format("YYYY-MM-DD");
+	const handleChanges = event => {
+		if (
+			event.target.name === 'zip' ||
+			event.target.name === 'goalAmount' ||
+			event.target.name === 'amountFunded' ||
+			event.target.name === 'duration'
+		) {
+			setProjectDetails({ ...projectDetails, [event.target.name]: Number(event.target.value) });
+		} else {
+			setProjectDetails({ ...projectDetails, [event.target.name]: event.target.value });
+		}
+	};
 
-const CreateProjectWizard = ({history}) => {
-    const [ projectDetails, setProjectDetails ] = useState({ 
-        name: '', 
-        startDate: currentDate, 
-        country: 'USA', 
-        duration: 1, 
-        description: '',
-        address: '', 
-        city: '',
-        state: '', 
-        zip: null,  
-        goalAmount: 0.00, 
-        amountFunded: 0.00, 
-        difficulty: '', 
-        images: []
-    })
-    const [formPosition, setFormPosition] = useState(1);
-    const [addProject] = useMutation(ADD_PROJECT);
+	const submitForm = async event => {
+		console.log('Project details ', projectDetails);
+		event.preventDefault();
+		const addedProj = await addProject({ variables: { data: projectDetails } });
+		if (addedProj) {
+			history.push(`/project/${addedProj.data.createProject.id}`);
+		}
+	};
 
-    const handleChanges = event => {
-        if (event.target.name === 'zip' || event.target.name === 'goalAmount' || event.target.name === 'amountFunded' || event.target.name === 'duration') {
-            setProjectDetails({ ...projectDetails, [event.target.name]: Number(event.target.value) })
-        } else {
-            setProjectDetails({ ...projectDetails, [event.target.name]: event.target.value })
-        }
-    };
+	return (
+		<>
+			<Nav />
+			<div className="create-project-page">
+				<div className="form-plus-quote-container">
+					<q className="quote">
+						<h5>
+							“Yesterday I was clever,
+							<br />
+							so I wanted to change the world
+							<br />
+							Today I am wise,
+							<br />
+							so I am changing myself.”
+						</h5>
+						<p>Jalal ad-Din Rumi PERSIAN POET</p>
+					</q>
 
-    const submitForm = async event => {
-        console.log("Project details ",projectDetails);
-        event.preventDefault();
-        const addedProj = await addProject({ variables: { data: projectDetails }})
-        if (addedProj) {
-            history.push(`/project/${addedProj.data.createProject.id}`)
-        }
-    }
+					<div className="ui-section">
+						<div className="progress-tracker">
+							<h1 className="title">Create Project</h1>
+							<div className="tracker">
+								<div className={formPosition >= 1 ? `step active` : `step`}></div>
+								<div className={formPosition >= 2 ? `step active` : `step`}></div>
+								<div className={formPosition >= 3 ? `step active` : `step`}></div>
+							</div>
+						</div>
 
-    return (
-        <>
-            <Nav />
-            <div className="create-project-page">
-                <div className="form-plus-quote-container">
-
-                    <q className="quote">
-                        <h5>“Yesterday I was clever, 
-                            <br />so I wanted to change the world 
-                            <br />Today I am wise, 
-                            <br />so I am changing myself.”
-                        </h5>
-                        <p>Jalal ad-Din Rumi PERSIAN POET</p>
-                    </q>
-                    
-
-                    <div className="ui-section">
-                        <div className="progress-tracker">
-                            <h1 className="title">Create Project</h1>
-                            <div className="tracker">
-                                <div className={formPosition >= 1 ? `step active` : `step`} ></div>
-                                <div className={formPosition >= 2 ? `step active` : `step`}></div>
-                                <div className={formPosition >= 3 ? `step active` : `step`}></div>
-                            </div>
-                        </div>
-
-                        {formPosition === 1
-                            ? <Form1
-                                setFormPosition={setFormPosition}
-                                handleChanges={handleChanges}
-                                name={projectDetails.name}
-                                startDate={projectDetails.startDate}
-                                description={projectDetails.description}
-                            />
-                            : formPosition === 2
-                            ? <Form2
-                                setFormPosition={setFormPosition}
-                                handleChanges={handleChanges}
-                                address={projectDetails.address}
-                                city={projectDetails.city}
-                                state={projectDetails.state}
-                                zip={projectDetails.zip}
-                                // projOwnerName={projectDetails.projOwnerName}
-                            />
-                            : formPosition === 3
-                            ? <Form3
-                                setFormPosition={setFormPosition}
-                                handleChanges={handleChanges}
-                                submitForm={submitForm}
-                                setProjectDetails={setProjectDetails}
-                                projectDetails={projectDetails}
-                                duration={projectDetails.duration}
-                                goalAmount={projectDetails.goalAmount}
-                                difficulty={projectDetails.difficulty}
-                                images={projectDetails.images}
-                                // amountFunded={projectDetails.amountFunded}
-                            />
-                            : null
-                        }
-                    </div>
-
-                </div>
-            </div>
-        </>
-    );
+						{formPosition === 1 ? (
+							<Form1
+								setFormPosition={setFormPosition}
+								handleChanges={handleChanges}
+								name={projectDetails.name}
+								startDate={projectDetails.startDate}
+								description={projectDetails.description}
+							/>
+						) : formPosition === 2 ? (
+							<Form2
+								setFormPosition={setFormPosition}
+								handleChanges={handleChanges}
+								address={projectDetails.address}
+								city={projectDetails.city}
+								state={projectDetails.state}
+								zip={projectDetails.zip}
+								// projOwnerName={projectDetails.projOwnerName}
+							/>
+						) : formPosition === 3 ? (
+							<Form3
+								setFormPosition={setFormPosition}
+								handleChanges={handleChanges}
+								submitForm={submitForm}
+								setProjectDetails={setProjectDetails}
+								projectDetails={projectDetails}
+								duration={projectDetails.duration}
+								goalAmount={projectDetails.goalAmount}
+								difficulty={projectDetails.difficulty}
+								images={projectDetails.images}
+								// amountFunded={projectDetails.amountFunded}
+							/>
+						) : null}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default withRouter(CreateProjectWizard);
