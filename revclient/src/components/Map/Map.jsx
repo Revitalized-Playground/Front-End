1e8455518074993496ced48b226861c263eb81cc
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-10-03 10:49:10 -0700

    recloned and reinstalled npm and added .env

--- revclient/src/components/Map/Map.jsx
+++ revclient/src/components/Map/Map.jsx
@@ -2,45 +2,39 @@ import React, { useState } from 'react';
 import MapGL, { GeolocateControl, Marker } from 'react-map-gl';
 // import 'mapbox-gl/dist/mapbox-gl.css'
 
-
-
-const TOKEN = "pk.eyJ1IjoicmV2aXRhbGl6ZXRlYW0iLCJhIjoiY2sxNm9wbng4MTgwbzNtbW9leXMzYTNpZSJ9.X4VRU_njmnvNxhnuhPZDHw"
-
+const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const projects = [
 	{
 		title: 'Alger Theatre',
 		description:
 			'Learn next to master trade professionals that are available during the day and after hours. Learn next to master trade Learn next to master trade professionals that are available during the day and after hours. Learn next to master trade',
- 
-        coordinates: [42.403611, -82.937222]
+
+		coordinates: [42.403611, -82.937222],
 	},
 	{
 		title: 'Fox Theatre Building',
 		description: 'Learn next to master trade professionals that are available during the day and after hours.',
-       
-        coordinates: [42.338333, -83.0525]
+
+		coordinates: [42.338333, -83.0525],
 	},
 	{
 		title: 'Detroit Masonic Temple',
 		description: 'Learn next to master trade professionals that are available during the day and after hours. ',
-        coordinates: [42.341667, -83.060278]
-       
+		coordinates: [42.341667, -83.060278],
 	},
 	{
 		title: 'Detroit Opera House',
 		description: 'Learn next to master trade professionals that are available during the day and after hours.',
-		coordinates: [42.336389, -83.048611]
+		coordinates: [42.336389, -83.048611],
 	},
 	{
 		title: 'Guidr Project',
 		description: 'Learn next to master trade professionals that are available during the day and after hours.',
-	
 	},
 	{
 		title: 'Clover 4',
 		description: 'Learn next to master trade professionals that are available during the day and after hours.',
-		
 	},
 	{
 		title: 'Project 7',
@@ -54,44 +48,40 @@ const projects = [
 	},
 ];
 
-
 const geolocateStyle = {
-    float: 'left',
-    margin: '50px',
-    padding: '10px'
+	float: 'left',
+	margin: '50px',
+	padding: '10px',
 };
 
 const Map = () => {
+	const [viewport, setViewPort] = useState({
+		width: 850,
+		height: 550,
+		latitude: 36.230099,
+		longitude: -101.887639,
+		zoom: 2,
+	});
 
-    const [viewport, setViewPort] = useState({
-        width: 850,
-        height: 550,
-        latitude: 36.230099,
-        longitude: -101.887639,
-        zoom: 2
-    })
+	const _onViewportChange = viewport => setViewPort({ ...viewport, transitionDuration: 50 });
 
-    const _onViewportChange = viewport => setViewPort({ ...viewport, transitionDuration: 50 })
-
-    const [Project] = useState(projects);
-    return (
-        <div style={{ margin: '0 auto' }}>
-
-            <MapGL
-                {...viewport}
-                mapboxApiAccessToken={TOKEN}
-                mapStyle="mapbox://styles/mapbox/dark-v8"
-                onViewportChange={_onViewportChange}
-            >
-                <GeolocateControl
-                    style={geolocateStyle}
-                    positionOptions={{ enableHighAccuracy: true }}
-                    trackUserLocation={true}
-                />
-
-            </MapGL>
-        </div>
-    )
-}
+	const [Project] = useState(projects);
+	return (
+		<div style={{ margin: '0 auto' }}>
+			<MapGL
+				{...viewport}
+				mapboxApiAccessToken={TOKEN}
+				mapStyle="mapbox://styles/mapbox/dark-v8"
+				onViewportChange={_onViewportChange}
+			>
+				<GeolocateControl
+					style={geolocateStyle}
+					positionOptions={{ enableHighAccuracy: true }}
+					trackUserLocation={true}
+				/>
+			</MapGL>
+		</div>
+	);
+};
 
 export default Map;
