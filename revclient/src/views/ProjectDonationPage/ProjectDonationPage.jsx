3765b6b603943532d4a98442effe2a55a0b034e3
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-03 17:37:37 -0700

    Scaffold ProjectDonationPage

---
+++ revclient/src/views/ProjectDonationPage/ProjectDonationPage.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+
+const ProjectDonationPage = props => {
+	// useAuth(props.history);
+
+	return <div>hello</div>;
+};
+
+export default withRouter(ProjectDonationPage);
--- revclient/src/views/ProjectPage/ProjectDescription/Donate/Donate.jsx
+++ revclient/src/views/ProjectPage/ProjectDescription/Donate/Donate.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components';
- 
+import { Link, withRouter } from 'react-router-dom';
 
 
-const Donate = ({raised, budget, donors, setModal}) => {
+const Donate = ({raised, budget, donors, setModal, match}) => {
     const percent = Number(raised) / Number(budget) * 100
     const Box = styled.div`
         height: 12px;
@@ -18,9 +18,9 @@ const Donate = ({raised, budget, donors, setModal}) => {
                   }
               100% {
                   width: ${raised > budget ? 100 : percent}%;
-          }              
+          }
         };
-    
+
     width: ${raised > budget ? 100 : percent}%;
     @keyframes progress-bar-stripes {
         from  { background-position: 40px 0; }
@@ -30,12 +30,12 @@ const Donate = ({raised, budget, donors, setModal}) => {
         ${percent < 100 ? `background-image: -webkit-linear-gradient(45deg, rgba(255,255,255,.15) 25%, transparent 25%, transparent 50%, rgba(255,255,255,.15) 50%, rgba(255,255,255,.15) 75%, transparent 75%, transparent);
         background-image: -o-linear-gradient(45deg, rgba(255,255,255,.15) 25%, transparent 25%, transparent 50%, rgba(255,255,255,.15) 50%, rgba(255,255,255,.15) 75%, transparent 75%, transparent);
         background-image: linear-gradient(45deg, rgba(255,255,255,.15) 25%, transparent 25%, transparent 50%, rgba(255,255,255,.15) 50%, rgba(255,255,255,.15) 75%, transparent 75%, transparent);
-        background-size: 40px 40px;` 
-        : 
+        background-size: 40px 40px;`
+        :
         `background-size: 40px 40px;`}
-        
+
         animation: ${percent >= 100 ? '1s ease-out pulse;' : '1s ease-out pulse, progress-bar-stripes 1s linear infinite;'};
-    
+
   `
 
     function numberWithCommas(x) {
@@ -54,7 +54,9 @@ const Donate = ({raised, budget, donors, setModal}) => {
                 <p className='donatorCount'>{formatter(donors)}</p>
                 <p className='donorText'>Donors</p>
                 <div className='donationButtons'>
-                    <button className='purple'>Donate now</button>
+                    <Link to={`/project/donate/${match.params.id}`}>
+                        <button className="purple">Donate now</button>
+                    </Link>
                     <button className='white' onClick={() => setModal(true)}>Share</button>
                 </div>
                 <div className="mid-line-container">
@@ -65,12 +67,12 @@ const Donate = ({raised, budget, donors, setModal}) => {
                 <div className='apply-button'>
                     <button >Apply to Project</button>
                 </div>
-                
+
                 <p className='lastText'>Partner with growing donors who are eager to see the transformation and economical growth of Detroit.</p>
             </div>
-            
+
         </div>
     )
 }
 
-export default Donate
+export default withRouter(Donate);
