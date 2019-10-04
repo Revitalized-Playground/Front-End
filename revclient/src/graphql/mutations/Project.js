66ed5e6d0acf9fa16957900ad7efeb1c7b025ab6
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-03 20:23:06 -0700

    Create project donation mutation

--- revclient/src/graphql/mutations/Project.js
+++ revclient/src/graphql/mutations/Project.js
@@ -1,26 +1,32 @@
 import { gql } from 'apollo-boost';
 
-
 export const ADD_COMMENT = gql`
-    mutation($data: CreateProjectCommentInput!) {
-        createProjectComment(data: $data) {
-            id
-            comment
-            profile {
-                id
-                firstName
-                lastName
-                profileImage
-            } 
-        }
-    }
+	mutation($data: CreateProjectCommentInput!) {
+		createProjectComment(data: $data) {
+			id
+			comment
+			profile {
+				id
+				firstName
+				lastName
+				profileImage
+			}
+		}
+	}
 `;
 
 export const ADD_PROJECT = gql`
-    mutation($data: CreateProjectInput!) {
-        createProject(data: $data) {
-            id
-        }
-    }
-`
+	mutation($data: CreateProjectInput!) {
+		createProject(data: $data) {
+			id
+		}
+	}
+`;
 
+export const DONATE_TO_PROJECT = gql`
+	mutation($id: ID!, $data: CreateProjectDonationInput!) {
+		createProjectDonation(id: $id, data: $data) {
+			id
+		}
+	}
+`;
