d4c76429eb823aadc2fdf08f31e8ddcef4670e0c
Frank Martinez <frankie.martinez@gmail.com>
2019-10-08 13:21:46 -0700

    ✨ New dashboard
    Working progress

--- revclient/src/graphql/queries/Users.js
+++ revclient/src/graphql/queries/Users.js
@@ -27,3 +27,83 @@ export const GET_USER = gql`
 
     }
 `;
+
+
+
+
+export const GET_USER_PROFILE = gql`
+    {
+        me {
+            id
+            email
+            firstName
+            lastName
+            profileImage
+            city
+            zip
+            address
+            state
+            aptNumber
+            donations {
+                id
+                amount
+            }
+            projects { 
+                id
+                name
+                description
+                address
+                state
+                zip
+                city
+                goalAmount
+                duration
+                difficulty
+                startDate
+                featuredImage
+                donations {
+                    id
+                    amount
+                }
+            }
+            likedProjects {
+                id
+                project {
+                    id
+                    name
+                }
+            }
+            comments {
+                id
+                comment
+                likes {
+                id
+                }
+                project {
+                    id
+                    name
+                    featuredImage
+                }
+            }
+            likedComments {
+                id
+                comment {
+                    id
+                    comment 
+                    profile {
+                        id
+                        firstName
+                        profileImage
+                    }
+                    project {
+                        id
+                        name
+                        featuredImage
+                    }
+                }
+            }
+        }
+    }
+
+
+`;
--- revclient/src/helpers/helpers.js
+++ revclient/src/helpers/helpers.js
@@ -5,3 +5,31 @@ export const zipCodeChecker = (e) => {
     }
     return false;
 }
+
+export function formatMoney(amount, decimalCount = 2, decimal = ".", thousands = ",") {
+    try {
+        decimalCount = Math.abs(decimalCount);
+        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+        const negativeSign = amount < 0 ? "-" : "";
+
+        let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+        let j = (i.length > 3) ? i.length % 3 : 0;
+
+        return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands);
+    } catch (e) {
+        console.log(e)
+    }
+};
+
+
+
+export function donationCount(num) {
+    return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
+};
+
+export const addUpDonations = (donationArray) => {
+    let totalDonations = 0
+    donationArray.map(donation => totalDonations = totalDonations + donation.amount);
+    return totalDonations;
+}
