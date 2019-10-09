4edc4ea133b2bd06a5eacf0bc0a30cb5249b27bc
Frank Martinez <frankie.martinez@gmail.com>
2019-10-09 15:12:41 -0700

    💄 One more little thing
    Put the search section back in the landing div since I'm no longer controlling width from the parent.

--- revclient/src/views/Landing/LandingPage.jsx
+++ revclient/src/views/Landing/LandingPage.jsx
@@ -33,8 +33,8 @@ export default function LandingPage() {
 			{/* <OnTheJob /> */}
 			{/* <FullScaleApp /> */}
 			{/* <Testimonials /> */}
+			<SearchProjects />
 		</div>
-		<SearchProjects />
 		<Footer />
 		</>
 	);
