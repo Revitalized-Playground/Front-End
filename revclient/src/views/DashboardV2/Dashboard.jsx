b2a4ef24fcafec1f9d01b275f73dc90095423145
Clark Williams <theclarkwilliams@gmail.com>
2019-10-11 13:03:25 -0600

    fixed issue with header not displaying when mapping through the combined 4 arrays

--- revclient/src/views/DashboardV2/Dashboard.jsx
+++ revclient/src/views/DashboardV2/Dashboard.jsx
@@ -13,32 +13,62 @@ export default function Dashboard() {
     
     const [selectedProject, setSelectedProject] = useState();
     const [toggle, setToggle] = useState(false);
+    const [array, setArray] = useState([]);
 
     const { loading, error, data, refetch } = useQuery(GET_USER_PROFILE);
-    
+
     useEffect(() => {
         refetch();
-    }, [])
-    
+        if(data) {
+            data.me.projects.map(project => {
+                project.type = "admin";
+                setArray(array => [...array, project])
+            })
+            data.me.donations.map(project => {
+                project.type = "donation";
+                setArray(array => [...array, project])
+            })
+            data.me.studentProjects.map(project => {
+                project.type = "student";
+                setArray(array => [...array, project])
+            })
+            data.me.tradeMasterProjects.map(project => {
+                project.type = "master";
+                setArray(array => [...array, project])
+            })
+        }
+    }, [data])
+
     if (loading) return <p>loading....</p>;
     if (error) return <p>Error....</p>;
 
     return (
         <>
-
             <div className="dashboard-container" >
                 <Nav />
-
                     <section className="dashboard">
                         <Sidebar user={data.me} />
-
                         <section className="dashboard-body">
-
                             { // Renders the project admin components
                                 toggle
-                                ? data.me.projects.map(project => {
-                                    if (project.id === selectedProject){
-                                        return <>
+                                ? 
+                                    array.map(project => {
+                                        if (project.id === selectedProject){
+                                            return <>
+                                                <Header
+                                                    key={project.id}
+                                                    project={project}
+                                                    setSelectedProject={setSelectedProject}
+                                                    setToggle={setToggle}
+                                                    toggle={toggle}
+                                                />
+                                                <Main project={project} />
+                                            </>
+                                        }
+                                    })
+                                :
+                                    array.map(project => (
+                                        <>
                                             <Header
                                                 key={project.id}
                                                 project={project}
@@ -46,28 +76,11 @@ export default function Dashboard() {
                                                 setToggle={setToggle}
                                                 toggle={toggle}
                                             />
-                                            <Main />
                                         </>
-                                    }
-                                })
-                                :
-                                data.me.projects.map(project => (
-                                    <>
-                                        {console.log(project)}
-                                        <Header
-                                            key={project.id}
-                                            project={project}
-                                            setSelectedProject={setSelectedProject}
-                                            setToggle={setToggle}
-                                            toggle={toggle}
-                                        />
-                                    </>
-                                ))
+                                    ))
                             }
-
                         </section>
-                    </section>
-                    
+                    </section>        
                 <Footer />
             </div>
 
