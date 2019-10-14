5f7f2a98f7452ef517875446aefb76a8c9720c1d
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-10-14 16:15:00 -0700

    fixed some bugs following pull

--- revclient/src/Routes.js
+++ revclient/src/Routes.js
@@ -1,142 +1,74 @@
-import React from 'react'
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 // Routes
-import LandingPage from "./views/Landing/LandingPage";
-import Register from "./views/Register/Register";
-import SetupProfile from "./views/SetupProfile/SetupProfile";
-import Login from "./views/Login/Login";
-import ProjectsHome from "./views/ProjectsHome/ProjectsHome";  // May be deprecated
+import LandingPage from './views/Landing/LandingPage';
+import Register from './views/Register/Register';
+import SetupProfile from './views/SetupProfile/SetupProfile';
+import Login from './views/Login/Login';
+import ProjectsHome from './views/ProjectsHome/ProjectsHome'; // May be deprecated
 
 // import Start from "./views/Start/Start";
-import Browse from "./views/Browse/Browse";
-import DashboardOld from "./views/DashboardOld/Dashboard";
-import Dashboard from "./views/DashboardV2/Dashboard";
-import CreateProjectWizard from './views/CreateProjWizard/CreateProjectWizard'
-import ProjectPage from './views/ProjectPage/ProjectPage'
+import Browse from './views/Browse/Browse';
+import DashboardOld from './views/DashboardOld/Dashboard';
+import Dashboard from './views/DashboardV2/Dashboard';
+import CreateProjectWizard from './views/CreateProjWizard/CreateProjectWizard';
+import ProjectPage from './views/ProjectPage/ProjectPage';
 import ProjectDonationPage from './views/ProjectDonationPage/ProjectDonationPage';
 import About from './views/About/About';
 
 // Utils
-import AuthenticateUser from "./utils/AuthenticateUser";
+import AuthenticateUser from './utils/AuthenticateUser';
 
-import StudentApplicationForm from './views/StudentApplicationForm/StudentApplicationForm'
-
-
+import StudentApplicationForm from './views/StudentApplicationForm/StudentApplicationForm';
 
 export const Routes = () => {
+	return (
+		<BrowserRouter>
+			{/* public routes */}
 
-    return (
-        <BrowserRouter>
+			<Route exact path="/" component={LandingPage} />
 
-            {/* public routes */}
+			<Route exact path="/register" component={Register} />
 
-            <Route
-                exact
-                path="/"
-                component={LandingPage }
-		    />
+			<Route exact path="/login" component={Login} />
 
-            <Route
-                exact
-                path="/register"
-                component={Register }
-            />
+			<Route exact path="/oauth/:token" component={AuthenticateUser} />
 
-            <Route
-                exact
-                path="/login"
-                component={Login }
-            />
+			<Route exact path="/about" component={About} />
 
-            <Route
-                exact
-                path="/oauth/:token"
-                component={AuthenticateUser}
-		    />
+			{/* private routes */}
 
-            <Route
-                exact
-                path="/about"
-                component={About }
-            />
+			<Route
+				// exact
+				path="/dashboard"
+				component={Dashboard}
+			/>
 
-
-            {/* private routes */}
-
-            <Route
-                // exact
-                path="/dashboard-old"
-                component={DashboardOld }
-            />
-
-
-            <Route
-                // exact
-                path="/dashboard"
-                component={Dashboard}
-
-            />
-
-            {/* <Route
+			{/* <Route
                 // exact
                 path="/profile/setup"
                 component={SetupProfile }
             /> */}
 
+			<Route
+				// exact
+				path="/settings"
+				render={() => <SetupProfile destination="settings" />}
+			/>
 
-            <Route
-                // exact
-                path="/settings"
-                render={() => (
-                    <SetupProfile destination="settings" />
-                )}
-            />
+			{/* Project routes */}
+			<Route path="/projects" component={ProjectsHome} />
 
+			<Route exact path="/project/:slug" component={ProjectPage} />
 
-            {/* Project routes */}
-            <Route
-                path="/projects"
-                component={ProjectsHome }
-            />
+			<Route exact path="/project/donate/:id" component={ProjectDonationPage} />
 
+			<Route path="/createproject" component={CreateProjectWizard} />
 
-            <Route
-                exact
-                path="/project/:slug"
-                component={ProjectPage }
-            />
+			<Route path="/browse" component={Browse} />
 
-
-
-            <Route
-                exact
-                path="/project/donate/:id"
-                component={ProjectDonationPage}
-            />
-
-            <Route
-                path="/createproject"
-                component={CreateProjectWizard }
-            />
-
-
-            <Route
-                path="/browse"
-                component={Browse }
-            />
-
-            <Route
-                exact
-                path='/studentapplicationform'
-                render={() => (
-                    <StudentApplicationForm />
-                )}
-            />
-
-        </BrowserRouter>
-    );
-}
-
-
-
+			<Route exact path="/studentapplicationform" render={() => <StudentApplicationForm />} />
+		</BrowserRouter>
+	);
+};
