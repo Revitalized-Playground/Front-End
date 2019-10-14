b9fb84b8ca211474c377e2511bbff71697d521d2
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-14 13:59:49 -0700

    Update project notation path

--- revclient/src/views/ProjectPage/ProjectDescription/ProjectComments/SingelComment.jsx
+++ revclient/src/views/ProjectPage/ProjectDescription/ProjectComments/SingelComment.jsx
@@ -1,76 +1,83 @@
-import React, {useState, useEffect} from 'react'
-import { FaRegEdit } from "react-icons/fa";
+import React, { useState, useEffect } from 'react';
+import { FaRegEdit } from 'react-icons/fa';
 
+const SingleComment = ({ each, editCommentMutation, deleteComment, currentUser, settings, settingsBlur, bool }) => {
+	const [comment, setComment] = useState(each);
+	const [editComment, setEditComment] = useState({ comment: comment.comment, id: comment.id });
+	const [eachSetting, setSettings] = useState(settings);
+	const [editing, setEditing] = useState(false);
 
-const SingleComment = ({each, editCommentMutation, deleteComment, currentUser, settings, settingsBlur, bool}) => {
-    const [comment, setComment] = useState(each)
-    const [editComment, setEditComment] = useState({comment: comment.comment, id: comment.id})
-    const [eachSetting, setSettings] = useState(settings)
-    const [editing, setEditing] = useState(false)
+	useEffect(() => {
+		setSettings(settings);
+	}, [bool]);
 
-    useEffect(() => {
-        setSettings(settings)
-    }, [bool])
+	const editCommentHandle = e => {
+		setEditComment({ ...editComment, comment: e.target.value });
+	};
+	const submitEditComment = async e => {
+		e.preventDefault();
 
-    const editCommentHandle = e => {
-        setEditComment({...editComment, comment: e.target.value})
-    }
-    const submitEditComment = async e => {
-        e.preventDefault()
+		const edited = await editCommentMutation({ variables: { data: editComment } });
+		if (edited) {
+			setComment({ ...each, comment: edited.data.updateProjectComment.comment });
+		}
+	};
 
-        const edited = await editCommentMutation({variables: {data: editComment}})
-        if(edited) {
-            setComment({...each, comment:edited.data.updateProjectComment.comment })
-        }
-    }
+	return (
+		<div className="commentFlex" key={comment.id}>
+			{comment.profile.profileImage === undefined ? null : (
+				<img src={comment.profile.profileImage} alt="Profile icon" />
+			)}
+			<div className="comment-inner-container">
+				<div className="commenter-name-flex">
+					<p>{comment.profile.firstName}</p>
+					<p>{comment.profile.lastName}</p>
+				</div>
+				<div className="dot-position-div">
+					<div className="each-comment-container">
+						{!editing ? (
+							<p className="comment">{comment.comment}</p>
+						) : (
+							<div className="lowerCommentSide">
+								<form>
+									<textarea
+										placeholder="edit"
+										onChange={editCommentHandle}
+										value={editComment.comment}
+									/>
+									<div className="edit-form-buttons">
+										<button
+											onClick={e => {
+												submitEditComment(e);
+												setEditing(false);
+											}}
+										>
+											Save
+										</button>{' '}
+										<button onClick={() => setEditing(false)}>Cancel</button>
+									</div>
+								</form>
+							</div>
+						)}
+					</div>
+					<div className="settings-drop" onClick={() => setSettings(!eachSetting)}>
+						<div className="dot-container">
+							<div onClick={() => setSettings(!eachSetting)} className="dot"></div>
+							<div className="dot"></div>
+							<div className="dot"></div>
+						</div>
+						{comment.profile.id === currentUser().profileId && eachSetting ? (
+							<div className="settings-drop-buttons">
+								<button onClick={() => setEditing(true)}>Edit</button>
+								<button onClick={e => deleteComment(e, comment.id)}>Delete</button>
+							</div>
+						) : null}
+					</div>
+					{/* <FaRegEdit onClick={submitEditComment} style={{fontSize: '2rem', color:'red'}}/> */}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-    console.log(comment);
-    
-    return (
-        <div className='commentFlex' key={comment.id}>
-            {comment.profile.profileImage === undefined ? null : (
-                <img src={comment.profile.profileImage} alt='Profile icon' />
-            )}
-            <div className='comment-inner-container'>
-                <div className='commenter-name-flex'>
-                    <p>{comment.profile.firstName}</p>
-                    <p>{comment.profile.lastName}</p>
-                </div>
-                <div className='dot-position-div'>
-                    <div className='each-comment-container'>
-                        {!editing 
-                            ? 
-                        <p className='comment'>{comment.comment}</p>
-                            :
-                        <div className='lowerCommentSide'>
-                            <form>
-                                <textarea
-                                placeholder='edit'
-                                onChange={editCommentHandle}
-                                value={editComment.comment}
-                                />
-                                <div className='edit-form-buttons'>
-                                    <button onClick={(e) => {submitEditComment(e); setEditing(false)}}>Save</button> <button onClick={() => setEditing(false)}>Cancel</button>
-                                </div>
-                                
-                            </form>
-                        </div>}
-                    </div>
-                    <div className='settings-drop' onClick={() => setSettings(!eachSetting)}>
-                        <div className='dot-container'><div onClick={() => setSettings(!eachSetting)} className='dot'></div><div className='dot'></div><div className='dot'></div></div>
-                    {comment.profile.id === currentUser().profileId && eachSetting 
-                        ? 
-                    <div className='settings-drop-buttons'><button onClick={() => setEditing(true)}>Edit</button><button onClick={(e) => deleteComment(e, comment.id)}>Delete</button></div>
-                        :
-                    null
-                    }
-                    </div>
-                    {/* <FaRegEdit onClick={submitEditComment} style={{fontSize: '2rem', color:'red'}}/> */}
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default SingleComment
-
+export default SingleComment;
