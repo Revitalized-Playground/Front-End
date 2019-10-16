00dd8fbd38753adc2d201b2c0a0ac95cc81a32d3
Jose Montero <josemariodev@gmail.com>
2019-10-16 10:09:06 -0500

    pull staging

--- revclient/src/views/DashboardV2/DashboardComponents/Main/Trades/Trades.jsx
+++ revclient/src/views/DashboardV2/DashboardComponents/Main/Trades/Trades.jsx
@@ -1,62 +1,51 @@
 import React, { useState, useEffect } from 'react';
 // import { FaLightbulb, FaFire, FaTasks FaWrench, FaCheck, FaRegClipboard } from "react-icons/fa";
-import { FaWrench, FaTasks } from "react-icons/fa";
-import Toggle from "react-toggle";
+import { FaWrench, FaTasks } from 'react-icons/fa';
+import Toggle from 'react-toggle';
 
-import AddTask from "../AddTask/AddTask";
-
+import AddTask from '../AddTask/AddTask';
 
 const ProjectTrades = props => {
-    const { trade, project } = props;
+	const { trade, project } = props;
+	console.log('project: ', project);
 
-    const [ state, setState ] = useState(false);
-    const [ addTaskModal, setAddTaskModal ] = useState({ show: false })
+	const [state, setState] = useState(false);
+	const [addTaskModal, setAddTaskModal] = useState({ show: false });
 
-
-    if (addTaskModal.show === true) {
+	if (addTaskModal.show === true) {
 		return (
-			<AddTask setAddTaskModal={setAddTaskModal} addTaskModal={addTaskModal} projectId={project.id} tradeId={trade.id} />
-		)
+			<AddTask
+				setAddTaskModal={setAddTaskModal}
+				addTaskModal={addTaskModal}
+				projectId={project.id}
+				tradeId={trade.id}
+			/>
+		);
 	}
 
-    // console.log("projectTrades props  ", props);
+	// console.log("projectTrades props  ", props);
 
-    return (
-        <>
+	return (
+		<>
+			<div className="trade-card-container">
+				<div className="trade-body">
+					<div className="trade-icon">
+						<FaWrench />
+					</div>
 
+					<div className="trade-detail description">
+						<h3>{trade.name}</h3>
+						<p>{trade.description}</p>
+					</div>
 
-            <div className="trade-card-container">
-                
-                <div className="trade-body">
-                    <div className="trade-icon">
-                        <FaWrench />
-                    </div>
-                    
-                    <div className="trade-detail description">
-                        <h3>{trade.name}</h3>
-                        <p>{trade.description}</p>
-                    </div>
-
-                    <div className="trade-detail create-task">
-                        <Toggle 
-                            defaultChecked={state}
-                            onChange={() => setState(!state)}
-                        />
-                        <FaTasks 
-                            className="create-task-button" 
-                            onClick={() => setAddTaskModal({ show: true })}
-                        />
-                    </div>
-                    
-                </div>
-
-            </div>
-
-        </>
-    )
-    
-}
-
+					<div className="trade-detail create-task">
+						<Toggle defaultChecked={state} onChange={() => setState(!state)} />
+						<FaTasks className="create-task-button" onClick={() => setAddTaskModal({ show: true })} />
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default ProjectTrades;
-
