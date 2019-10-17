31e61c6299c5257cb94d6047397368bf3e767323
Clark Williams <theclarkwilliams@gmail.com>
2019-10-17 16:57:00 -0600

    added nav and footer to student application form

--- revclient/src/views/StudentApplicationForm/StudentApplicationForm.jsx
+++ revclient/src/views/StudentApplicationForm/StudentApplicationForm.jsx
@@ -8,6 +8,9 @@ import large from '../../assets/StudentApplicationWizard/large.svg'
 import med from '../../assets/StudentApplicationWizard/med.svg'
 import small from '../../assets/StudentApplicationWizard/small.svg'
 
+import Nav from '../../components/Layout/Nav';
+import Footer from '../../components/Layout/Footer';
+
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_PROJECT_BY_SLUG } from '../../graphql/queries/Projects'
 import { APPLY_TO_PROJECT } from '../../graphql/mutations/Project'
@@ -65,38 +68,42 @@ const ApplicationForm = (props) => {
 		);
     }
     return(
-        <div className='student-application-form-container'>
-            <div style={{backgroundImage: `url(${swirly})`}} className='swirly-div'>
-                <div  className='student-app-steps'>
-                    <h3 style={{color: step === 1 ? 'white' : null}}>Step 1</h3>
-                    <h3 style={{color: step === 2 ? 'white' : null}}>Step 2</h3>
-                    <h3 style={{color: step === 3 ? 'white' : null}}>Step 3</h3>
-                    <h3 style={{color: step === 4 ? 'white' : null}}>Step 4</h3>
-                    <h3 style={{color: step === 5 ? 'white' : null}}>Step 5</h3> 
+        <>
+            <Nav />
+                <div className='student-application-form-container'>
+                    <div style={{backgroundImage: `url(${swirly})`}} className='swirly-div'>
+                        <div  className='student-app-steps'>
+                            <h3 style={{color: step === 1 ? 'white' : null}}>Step 1</h3>
+                            <h3 style={{color: step === 2 ? 'white' : null}}>Step 2</h3>
+                            <h3 style={{color: step === 3 ? 'white' : null}}>Step 3</h3>
+                            <h3 style={{color: step === 4 ? 'white' : null}}>Step 4</h3>
+                            <h3 style={{color: step === 5 ? 'white' : null}}>Step 5</h3> 
+                        </div>
+                        <div className='student-app-images'>
+                            <img src={large} alt='' className='large' />
+                            <img src={med} alt='' className='med' />
+                            <img src={small} alt='' className='small' />
+                        </div>
+                    </div>
+                    <div className='right-div'>
+                        <div className='right-div-content'>
+                            {step === 1 
+                            ?
+                            <Step1 trades={data.projectBySlug.trades} setStep={setStep} chosenTrade={chosenTrade} setChosenTrade={setChosenTrade} obj={obj} setObj={setObj} errorHandle={errorHandle} setErrorHandle={setErrorHandle}/>
+                            : step === 2
+                            ?
+                            <Step2 setStep={setStep} obj={obj} setObj={setObj} errorHandle={errorHandle} setErrorHandle={setErrorHandle} />
+                            : step === 3
+                            ?
+                            <Step3 setStep={setStep} obj={obj} setObj={setObj} submit={submit} errorHandle={errorHandle} setErrorHandle={setErrorHandle} />
+                            :
+                            null
+                            }
+                        </div>
+                    </div>
                 </div>
-                <div className='student-app-images'>
-                    <img src={large} alt='' className='large' />
-                    <img src={med} alt='' className='med' />
-                    <img src={small} alt='' className='small' />
-                </div>
-            </div>
-            <div className='right-div'>
-                <div className='right-div-content'>
-                    {step === 1 
-                    ?
-                    <Step1 trades={data.projectBySlug.trades} setStep={setStep} chosenTrade={chosenTrade} setChosenTrade={setChosenTrade} obj={obj} setObj={setObj} errorHandle={errorHandle} setErrorHandle={setErrorHandle}/>
-                    : step === 2
-                    ?
-                    <Step2 setStep={setStep} obj={obj} setObj={setObj} errorHandle={errorHandle} setErrorHandle={setErrorHandle} />
-                    : step === 3
-                    ?
-                    <Step3 setStep={setStep} obj={obj} setObj={setObj} submit={submit} errorHandle={errorHandle} setErrorHandle={setErrorHandle} />
-                    :
-                    null
-                    }
-                </div>
-            </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
