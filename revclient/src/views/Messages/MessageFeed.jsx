5ab8654aca12d5ca0dc029453fc66a3d3c46f96a
Jose Montero <josemariodev@gmail.com>
2019-10-17 17:04:26 -0500

    styling wip

--- revclient/src/views/Messages/MessageFeed.jsx
+++ revclient/src/views/Messages/MessageFeed.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const MessageFeed = props => {
-	return <div>{props.user.message}</div>;
+	const { image, message } = props.user;
+	return (
+		<div className="feed-container">
+			<div className="img-container">
+				<img src={image} alt="user" />
+				<div className="date">date</div>
+			</div>
+			<div className="feed-message">{message}</div>
+		</div>
+	);
 };
 
 export default MessageFeed;
