8b66180b5d93e0f000efbaea5194bd4971a0d78e
Jose Montero <josemariodev@gmail.com>
2019-10-22 16:00:24 -0500

    Center loader

--- revclient/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ revclient/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
 
-
 const LoadingSpinner = () => {
+	return (
+		<section className="loading-spinner">
+			<Loader
+				type="Circles"
+				color="#4840ba"
+				height={100}
+				width={100}
+				timeout={3000} //3 secs
+			/>
+		</section>
+	);
+};
 
-    return (
-        <section className="loading-spinner">
-            <Loader
-                type="Circles"
-                color="#4840ba"
-                height={100}
-                width={100}
-                timeout={3000} //3 secs
-            />
-        </section>
-    );
-}
-
-
-export default LoadingSpinner
-
+export default LoadingSpinner;
